b4e15d17a5d2d52fb19601b0c87a5a11b04f6c7b
Mallick, Nancy <Nancy.Mallick@nordstrom.com>
2018-06-21 16:26:01 -0700

    removing unnecessary files

--- app/models/spaces.js
+++ app/models/spaces.js
@@ -1,24 +1,9 @@
-// def createBear(address, description, size){
-// 	console.log("Bear Created");
-// }
-
-// def getBear(bearID){
-// 	return {
-// 				address : "14263 SE 38th ST, Bellevue, WA, 98006"
-// 				,description: "Some Sample description"
-// 				,size: "1234 sqft"
-// 		};
-// }
-
-
-
 function getSpace(spaceID) {
 	return {
 				 ID: spaceID
-				,address : "263 SE 40th ST, Bellevue, WA, 98006"
+				,address : "40th ST, Bellevue, WA, 98006"
 				,description: "Some Sample description"
 				,size: "1234 sqft"
 		   };
 }
-
 module.exports.getSpace = getSpace;
